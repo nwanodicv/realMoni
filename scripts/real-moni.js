0b9c665ba6aaae7cf19e15b5e545fe0e897963ca
nwanodicv <victornwanodi@gmail.com>
2024-11-19 09:57:57 +0100

    first commit

---
+++ scripts/real-moni.js
@@ -0,0 +1,8 @@
+function myFunction() {
+	const x = document.querySelector(".nav-bar-container");
+	if (x.style.display === "block") {
+	  x.style.display = "none";
+	} else {
+	  x.style.display = "block";
+	}
+  }
